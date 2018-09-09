4fa7ec4abe222c3a2ba0f4cb1e0e7a344532f7ab
JackOfHart <41881007+JackOfHart@users.noreply.github.com>
2018-09-08 20:08:57 -0400

    New Version of 9/8 with broken spawner

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 
 
@@ -10,10 +11,22 @@ public class Player : MonoBehaviour {
     public float Speed;
     public float RotateSpeed;
     public float jumpHeight;
+    public Text score;
 
-    
+    public GameObject Cop;
+    public GameObject tashCan;
+    private GameObject HeldStuff;
+
+    private float distanceToCop;
+    private float distancetoDrop;
+    private bool DropExist = false;
 
     private Vector3 CameraRotate = new Vector3(0f, 0f, 0f);
+    private bool ControlsEnabled = true;
+    private float itemsHeld = 0;
+    private bool hasItems = true;
+    private int trashOnTheGround = 0;
+
     // Use this for initialization
     void Start () {
       
@@ -25,7 +38,7 @@ public class Player : MonoBehaviour {
 
         
 
-        if (Input.GetKey(KeyCode.A))
+        if (Input.GetKey(KeyCode.A) && ControlsEnabled == true)
         {
             transform.position += Vector3.left * Speed * Time.deltaTime;
             
@@ -33,7 +46,7 @@ public class Player : MonoBehaviour {
            //transform.Rotate(Vector3.down * RotateSpeed * Time.deltaTime);
         }
 
-        if (Input.GetKey(KeyCode.D))
+        if (Input.GetKey(KeyCode.D) && ControlsEnabled == true)
         {
             transform.position += Vector3.right * Speed * Time.deltaTime;
 
@@ -42,25 +55,74 @@ public class Player : MonoBehaviour {
         }
 
 
-        if (Input.GetKey(KeyCode.W))
+        if (Input.GetKey(KeyCode.W) && ControlsEnabled == true)
         {
             transform.localPosition += transform.forward * Speed * Time.deltaTime;
         }
 
-        if (Input.GetKey(KeyCode.S))
+        if (Input.GetKey(KeyCode.S) && ControlsEnabled == true) 
         {
           transform.localPosition += Vector3.back * Speed * Time.deltaTime;
         }
 
-        if (Input.GetKey(KeyCode.Space))
+        if (Input.GetKey(KeyCode.Space) && ControlsEnabled == true)
         {
             transform.position += Vector3.up * jumpHeight * Time.deltaTime;
         }
 
+
+        distanceToCop = Vector3.Distance(Cop.transform.position, this.transform.position);
+
+        if(DropExist == true)
+        {
+            //distancetoDrop = Vector3.Distance(HeldStuff.transform.position, this.transform.position);
+
+            if(distancetoDrop < 5 && Input.GetKey(KeyCode.LeftShift))
+            {
+                //DESTROYOBJECTHERE
+
+                TrashCan.score += 10;
+                score.text = "Score: " + TrashCan.score;
+
+                trashOnTheGround--;
+            }
+
+        }
         
+        if(trashOnTheGround <= 0)
+        {
+            DropExist = false;
+        }
 
 
+        //Spotted
+        if (distanceToCop <= 2)
+        {
+            ControlsEnabled = false;
 
+            
+
+            
+
+            this.transform.position += new Vector3(1, 1, 0) * Speed * Time.deltaTime;
+
+            if(hasItems == true)
+            {
+                DropItems();
+            }
+            
+        
+        }
+
+
+        else if(distanceToCop >4)
+        {
+            ControlsEnabled = true;
+            hasItems = true;
+        }
+
+
+        print(trashOnTheGround);
     }
 
     void OnCollisionEnter(Collision col)
@@ -70,8 +132,48 @@ public class Player : MonoBehaviour {
         {
             print("collide alley");
            //Camera.transform.rotation  = Quaternion.LookRotation(CameraRotate, new Vector3(0f,0f,0f));
-           // Camera.transform.position = new Vector3(5.31f, 3, 0); 
+           //Camera.transform.position = new Vector3(5.31f, 3, 0); 
         }
 
+    }
+
+    void DropItems()
+    {
+        
+
+        int i = 0;
+
+        itemsHeld = (TrashCan.score / 10);
+
+        Debug.Log(itemsHeld);
+
+        if(TrashCan.score > 0)
+        {
+            for (i = 0; i < itemsHeld; i++)
+            {
+                //SpawnPrefab
+                //GiveLocation
+
+                Debug.Log(HeldStuff);
+
+               
+
+              
+
+                
+
+                TrashCan.score = 0;
+
+                score.text = "Score: " + TrashCan.score;
+
+                DropExist = true;
+
+                trashOnTheGround++;
+            }
+
+
+        }
+
+        hasItems = false;
     }
 }
